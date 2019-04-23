f28aa64cdc6c57aae40cefb8f7373f133492c0ad
Orange81 <anika.vaarhorst@gmail.com>
2019-04-23 15:56:52 +0200

    Create exercise_0204.py

---
+++ exercise_0204.py
@@ -0,0 +1,6 @@
+# Start the Python interpreter and enter bruce + 4 at the prompt. This will give you an error:
+# NameError: name 'bruce' is not defined
+# Assign a value to bruce so that bruce + 4 evaluates to 10.
+
+bruce = 6
+bruce + 4
