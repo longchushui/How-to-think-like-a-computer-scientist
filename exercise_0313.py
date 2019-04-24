b6f4f4f562df3fa518e3299518a155abb95f7b56
Orange81 <anika.vaarhorst@gmail.com>
2019-04-24 15:57:36 +0200

    Create exercise_0313.py

---
+++ exercise_0313.py
@@ -0,0 +1,7 @@
+# Create a turtle, and assign it to a variable. When you ask for its type, what do you get?
+
+import turtle           # get the turtle module
+tess = turtle.Turtle()  # set up turtle, and assign to tess
+print(type(tess))       # ask for its type
+
+# you see that tess is of class turtle.Turtle
