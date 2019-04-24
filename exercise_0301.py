4c7e6e1511cee108ebb0291b25811ced52e9669a
Orange81 <anika.vaarhorst@gmail.com>
2019-04-24 14:09:34 +0200

    Create exercise_0301.py

---
+++ exercise_0301.py
@@ -0,0 +1,4 @@
+# Write a program that prints We like Python's turtles! 1000 times.
+
+for i in range(1000):
+    print("We like Python's turtles!") # use double quotes here, to prevent issues with the single quote
