b16362f44f52955d97037e5d163758c636c58a84
Orange81 <anika.vaarhorst@gmail.com>
2019-04-28 13:40:08 +0200

    Update and rename exercise_0511.py to exercise_0511_0512.py

--- exercise_0511_0512.py
+++ exercise_0511_0512.py
@@ -9,3 +9,16 @@ def is_rightangled(a, b, c):
     """
     return abs(c ** 2) == abs(a ** 2) + abs(b ** 2)
     
+# Extend the above program so that the sides can be given to the function in any order.
+
+def is_rightangled(a, b, c):
+    """ The function is_rightangled() will return True if the triangle is right-angled,
+        false otherwise. The values a, b and c represent the length of the sides of the
+        triangle.
+    """
+    if max(a, b, c) == a:
+        return abs(a ** 2) == abs(b ** 2) + abs(c ** 2)
+    elif max(a, b, c) == b:
+        return abs(b ** 2) == abs(a ** 2) + abs(c ** 2)
+    else:
+        return abs(c ** 2) == abs(a ** 2) + abs(b ** 2)
